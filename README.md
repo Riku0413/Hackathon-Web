14b25a3d1485b9015e322c9f1453661968be5b46
Riku0413 <riku.k.18.soccer@gmail.com>
2023-09-28 10:36:38 +0900

    Added codes of the front-end.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hackathon-Web
+
+## アプリの立ち上げ方
+
+`npm install react-scripts`
+
+モジュールをインストール
+
+`npm start`
+
+ブラウザでアプリを起動
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <div className="title">
+          User Register
+        </div>
+      </header>
+
+      <main>
+        <Form />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
